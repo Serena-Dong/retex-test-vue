88f90c8189e38b328d8322496f679e029a4a62ae
Serena-Dong <serenadong3159@icloud.com>
2024-06-03 05:41:03 +0200

    header structure and style

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,302 @@
+<script>
+import { headerData } from '../data/data';
+import Button from './contents/Button.vue';
+
+export default {
+    name: 'AppHeader',
+    components: { Button },
+    data() {
+        return {
+            data: headerData,
+            isClicked: false
+        }
+    },
+}
+</script>
+
+<template>
+    <header>
+        <div class="logo">
+            <!-- <img src="" alt=""> -->
+        </div>
+        <div class="header-top">
+            <div class="container">
+                <div class="row justify-content-between">
+
+                    <!-- Links -->
+                    <div class="col col-12 col-lg-10 d-flex justify-content-start">
+                        <div class="col col-6 col-lg-1 text-center">
+                            <a href="">Contribuisci</a>
+                        </div>
+                        <div class="col col-6 col-lg-1">
+                            <a href="">Abbonati</a>
+                        </div>
+                    </div>
+
+                    <!-- LOGIN -->
+                    <div id="login" class="col col-lg-1">
+                        <a href=""><i class="fa-regular fa-user mx-1"></i> Accedi</a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+        <div class="header-bottom">
+            <div class="container">
+                <div class="row">
+
+                    <!-- Menù -->
+                    <div class="col col-2 col-lg-1">
+                        <p class=""><i class="fa-solid fa-bars"></i></p>
+                    </div>
+                    <div class="col-8 col-lg-10"></div>
+                    <!-- Search Bar -->
+                    <div class="col col-2 col-lg-1">
+                        <p><i class="fa-solid fa-magnifying-glass"></i></p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- <div id="menu"></div> -->
+
+        <!-- Navbar -->
+        <div id="navbar">
+            <div class="container">
+                <div class="row align-items-center">
+
+                    <!-- Links -->
+                    <div class="col-10">
+                        <ul>
+                            <li><a class="mr-3 fw-semibold" href="">Tutti i temi</a></li>
+                            <li class="ms-4 fw-semibold" v-for="theme in data.navbar.themes">
+                                <a :href="theme.path">{{ theme.name }}</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- Buttons -->
+                    <div class="col-2 buttons text-end">
+                        <span>Aa</span>
+                        <span><i class="fa-regular fa-image"></i></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- Themes -->
+        <div id="themes">
+            <div class="container">
+                <div class="row d-flex flex-column align-items-start">
+
+                    <!-- Title -->
+                    <div class="col col-lg-4 title flex justify-content-start gap-3 mb-3">
+                        <h1 class="m-0"><b>Tutti i temi</b></h1><span><i
+                                class="fa-solid fa-chevron-down fs-2 text"></i></span>
+                    </div>
+
+                    <!-- Buttons -->
+                    <div class="col col-lg-8 buttons flex flex-wrap justify-content-start gap-2">
+                        <div v-for="theme in data.navbar.themes" class="button" :class="'bg-' + theme.bgColor">
+                            <a :href="theme.path">{{ theme.name }}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style scoped lang="scss">
+@import "./../assets/scss/partials/variables";
+
+header {
+    position: sticky;
+    top: 0;
+    z-index: 99999;
+
+    &>* {
+        background-color: $white;
+        border-bottom: 2px solid;
+        min-height: 50px;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .col {
+            min-height: 50px;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            p {
+                margin: 0;
+            }
+        }
+    }
+
+    .logo {
+        @media screen and (min-width: 992px) {
+            display: none;
+        }
+
+    }
+
+    .header-top {
+        .col .col {
+            &:first-child {
+                border-right: 2px solid;
+            }
+
+        }
+
+        #login {
+            display: none;
+        }
+
+        @media screen and (min-width: 992px) {
+            .col .col {
+                &:first-child {
+                    border-right: none;
+                }
+
+                a {
+                    font-size: 0.8rem;
+                }
+            }
+
+            #login {
+                display: flex;
+            }
+        }
+
+    }
+
+    .header-bottom {
+        position: relative;
+
+        .col {
+            height: 70px;
+
+            &:first-child {
+                border-right: 2px solid;
+            }
+
+            &:last-child {
+                border-left: 2px solid;
+            }
+
+            p {
+                cursor: pointer;
+
+                i {
+                    color: $red;
+                }
+            }
+        }
+    }
+
+    // #menu {
+    //     position: absolute;
+    //     top: calc(100% - 70px);
+
+    //     height: 88vh;
+    //     width: 100%;
+    //     border: solid;
+
+    //     background-color: red;
+    // }
+
+    #navbar {
+
+        @media screen and (max-width: 992px) {
+            display: none;
+        }
+
+        .row {
+            height: 70px;
+        }
+
+        ul {
+            display: flex;
+            justify-content: start;
+
+            li a:hover {
+                color: $black;
+                border-bottom: 5px solid $red;
+                padding-bottom: 21px;
+            }
+
+            li a.active {
+                border-bottom: 5px solid $red;
+                padding-bottom: 21px;
+            }
+        }
+
+        .buttons {
+            span {
+                margin-right: 1rem;
+                padding: 0.5rem;
+
+                cursor: pointer;
+            }
+
+            span.active {
+                border: 2px solid;
+                border-radius: 0.4rem;
+                box-shadow: -2px 2px 0 black;
+            }
+        }
+    }
+
+    #themes {
+        min-height: 150px;
+        background-color: $grey;
+
+        padding: 2rem 0;
+
+        .container .row {
+            @media screen and (min-width: 992px) {
+                flex-direction: row !important;
+            }
+        }
+
+        .title {
+            @media screen and (min-width: 992px) {
+                justify-content: flex-start !important;
+
+                .fa-chevron-down {
+                    display: none;
+                }
+            }
+        }
+
+        .buttons {
+            @media screen and (min-width: 992px) {
+                justify-content: flex-end !important;
+            }
+        }
+
+        .button {
+            color: black;
+            text-transform: uppercase;
+            font-size: 0.8rem;
+            font-weight: 500;
+            border: 1px solid black;
+            box-shadow: -2px 2px 0 black;
+            border-radius: 10%;
+
+            padding: 0.5rem 1rem;
+
+            @media screen and (max-width: 992px) {
+                background-color: $white !important;
+            }
+
+        }
+    }
+}
+</style>
